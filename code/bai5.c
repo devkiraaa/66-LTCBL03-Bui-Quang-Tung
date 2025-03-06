c25f7faeea41d7879445588ae7ff19136d59a1d7
devkiraaa <buitung2k6hy@gmail.com>
2025-03-06 09:02:36 +0700

    bai5.c

--- code/bai5.c
+++ code/bai5.c
@@ -15,8 +15,8 @@
                   printf("Phuong trinh vo nghiem");
             }
             else{
-                  x1 = (- b - sqrt(delta)) / 2 * a;
-                  x2 = (- b + sqrt(delta)) / 2 * a;
+                  x1 = (- b - sqrt(delta)) / (2 * a);
+                  x2 = (- b + sqrt(delta)) / (2 * a);
                   printf("%lf %lf", x1, x2);
             }
       }
